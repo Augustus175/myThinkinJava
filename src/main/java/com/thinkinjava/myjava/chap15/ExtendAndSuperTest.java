62c85f58f4db87b3cb272c5aab431cb3e5ca6fcd
Augustus175 <columbia7311@163.com>
2019-01-03 22:43:48 +0800

    2019-01-03 22:43:33

---
+++ src/main/java/com/thinkinjava/myjava/chap15/ExtendAndSuperTest.java
@@ -0,0 +1,27 @@
+package com.thinkinjava.myjava.chap15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Biology {
+}
+
+class Animal extends Biology {
+}
+
+class Dog extends Animal {
+}
+
+public class ExtendAndSuperTest {
+    public static void main(String[] args) {
+        List<? extends Animal> list1 = new ArrayList<>();
+//        list1.add(new Animal());
+//        list1.add(new Dog());
+
+        List<? super Animal> list2 = new ArrayList<>();
+        list2.add(new Animal());
+        list2.add(new Dog());
+//        list2.add(new Biology());
+
+    }
+}
