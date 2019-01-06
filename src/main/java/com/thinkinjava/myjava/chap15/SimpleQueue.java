c3f0498504cdc5bfeaa2fb4731cabc283a90051a
Augustus175 <columbia7311@163.com>
2019-01-06 23:01:09 +0800

    2019-01-06 23:00:46

---
+++ src/main/java/com/thinkinjava/myjava/chap15/SimpleQueue.java
@@ -0,0 +1,21 @@
+package com.thinkinjava.myjava.chap15;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SimpleQueue<T> implements Iterable<T> {
+    private LinkedList<T> storage = new LinkedList<>();
+
+    public void add(T t) {
+        storage.offer(t);
+    }
+
+    public T get() {
+        return storage.poll();
+    }
+
+    public Iterator<T> iterator() {
+        return storage.iterator();
+    }
+}
