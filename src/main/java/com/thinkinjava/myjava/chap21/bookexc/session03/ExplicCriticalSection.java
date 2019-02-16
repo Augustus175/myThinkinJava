c94585c4391b2f58ba1503e1800232f2c27393f3
gavin <columbia7311@163.com>
2019-02-16 23:33:42 +0800

    2019-02-16 23:32:49

--- src/main/java/com/thinkinjava/myjava/chap21/bookexc/session03/ExplicCriticalSection.java
+++ src/main/java/com/thinkinjava/myjava/chap21/bookexc/session03/ExplicCriticalSection.java
@@ -5,17 +5,11 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 class ExplicitPariManager1 extends PairManager {
-    private Lock lock = new ReentrantLock();
 
     public synchronized void increment() {
-        lock.lock();
-        try {
             p.incrementX();
             p.incrementY();
             store(getPair());
-        } finally {
-            lock.unlock();
-        }
     }
 }
 
