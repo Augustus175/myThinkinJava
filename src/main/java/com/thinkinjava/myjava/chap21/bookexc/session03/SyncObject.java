bdfef413d2802aab665c5f5ea78e68a9a0a34b93
gavin <columbia7311@163.com>
2019-02-21 23:51:08 +0800

    2019-02-21 23:50:27

--- src/main/java/com/thinkinjava/myjava/chap21/bookexc/session03/SyncObject.java
+++ src/main/java/com/thinkinjava/myjava/chap21/bookexc/session03/SyncObject.java
@@ -1,20 +1,30 @@
 package com.thinkinjava.myjava.chap21.bookexc.session03;
 
 class DualSynch {
-    private Object synOnject = new Object();
+    private Object synObject = new Object();
 
     public synchronized void f() {
         for (int i = 0; i < 5; i++) {
             System.out.println("f()");
             Thread.yield();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void g() {
-        synchronized (synOnject) {
+        synchronized (synObject) {
             for (int i = 0; i < 5; i++) {
                 System.out.println("g()");
                 Thread.yield();
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
--- src/main/java/com/thinkinjava/myjava/chap21/bookexc/session03/ThreadLoaclVariableHolder.java
+++ src/main/java/com/thinkinjava/myjava/chap21/bookexc/session03/ThreadLoaclVariableHolder.java
@@ -1,5 +1,10 @@
 package com.thinkinjava.myjava.chap21.bookexc.session03;
 
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 class Accessor implements Runnable {
     private final int id;
 
@@ -12,22 +17,43 @@ class Accessor implements Runnable {
             ThreadLoaclVariableHolder.increment();
             System.out.println(this);
             Thread.yield();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public String toString() {
-        return "#" + id + ": " + ThreadLoaclVariableHolder.get();
+        return "#" + id + " : " + ThreadLoaclVariableHolder.get();
     }
 }
 
 public class ThreadLoaclVariableHolder {
-    public static void main(String[] args) {
+    private static ThreadLocal<Integer> value =
+            new ThreadLocal<Integer>() {
+                private Random random = new Random(47);
+
+                protected synchronized Integer initialValue() {
+                    return random.nextInt(100000);
+                }
+            };
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(new Accessor(i));
+        }
+        TimeUnit.SECONDS.sleep(3);
+        executorService.shutdown();
     }
 
     public static void increment() {
+        value.set(value.get() + 1);
     }
 
     public static int get() {
-        return 0;
+        return value.get();
     }
 }
